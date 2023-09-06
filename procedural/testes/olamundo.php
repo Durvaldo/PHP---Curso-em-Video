bd256bacb107db39c9bd4c5dae127eeddaefd3f1
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2023-09-06 11:34:51 -0300

    Configuração de pasta

---
+++ procedural/testes/olamundo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio</title>
+</head>
+
+<body>
+    <?php
+    echo "<h1>Olá, Mundo!</h1>";
+    echo "<h1>Estou vendo os primeiros passos do php 8.1</h1>";
+    phpinfo();
+    ?>
+</body>
+
+</html>
